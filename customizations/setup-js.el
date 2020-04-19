1ba07660ef736cd87a378a5acfa38d92c44bff92
Philip White <phil_23@live.co.uk>
2020-04-19 14:01:29 +0100

    Add js config
    - js-2 mode
    - refactor-mode
    - js2-xref
    - prettier

--- customizations/setup-js.el
+++ customizations/setup-js.el
@@ -1,6 +1,19 @@
 ;; javascript / html
-(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
-(add-hook 'js-mode-hook 'subword-mode)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+(define-key js-mode-map (kbd "M-.") nil)
+(add-hook 'js2-mode-hook (lambda ()
+  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+
+(add-hook 'js2-mode-hook 'prettier-js-mode)
+
+(setq prettier-js-args ' (
+  ;; Add customisations here e.g "--trailing-comma" "all"
+))
+
 (add-hook 'html-mode-hook 'subword-mode)
 (setq js-indent-level 2)
 (eval-after-load "sgml-mode"
@@ -9,14 +22,5 @@
      (tagedit-add-paredit-like-keybindings)
      (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
 
+;; TODO - typescript
 
-;; coffeescript
-(add-to-list 'auto-mode-alist '("\\.coffee.erb$" . coffee-mode))
-(add-hook 'coffee-mode-hook 'subword-mode)
-(add-hook 'coffee-mode-hook 'highlight-indentation-current-column-mode)
-(add-hook 'coffee-mode-hook
-          (defun coffee-mode-newline-and-indent ()
-            (define-key coffee-mode-map "\C-j" 'coffee-newline-and-indent)
-            (setq coffee-cleanup-whitespace nil)))
-(custom-set-variables
- '(coffee-tab-width 2))
--- init.el
+++ init.el
@@ -148,5 +148,11 @@
  '(js-chain-indent t)
  '(package-selected-packages
    (quote
-    (solarized-theme company magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell))))
+    (prettier-js xref-js2 js2-refactor js2-mode solarized-theme company magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell))))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
